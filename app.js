d9a5869083b4379e18d9d5d623d7b37c77d55e13
Sean-Miningah <miningahsean@gmail.com>
2022-07-31 12:41:30 +0300

    Mongodb setup | Initial Model creation

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running ...."));
+const PORT = 3000
 
+// Middlewares
+app.use(express.json());
+
+
+// Database Connection
+await mongoose.connect(mongoString, {useNewUrlParser: true}, () => 
+  console.log('connected to DB!')
+);
+
+// App is running, port is open
+app.listen(PORT, () => console.log("Server is running ...."));
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 // Routes
 app.get('/', (req, res) => {
 	res.send("Hello World");
 });
 
-app.get('/posts', (req, res) => {
-	res.send('We are on Posts');
-});
